a2929b133bf99eb29b93bb17ca0d887850f795da
lucasherculiani <164363379+lucasherculiani@users.noreply.github.com>
2026-05-30 18:33:12 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "i9treino-pwa-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
